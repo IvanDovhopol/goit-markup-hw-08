9a2690b48f4736e43df4becb2e7bbbf3aa20de61
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-07-06 19:04:55 +0300

    add adaptive sec header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
 	const refs = {
-		openMenuBtn: document.querySelector('.menu-open-btn'),
-		closeMenuBtn: document.querySelector('.menu-close-btn'),
+		openMenuBtn: document.querySelector('.btn__menu--open'),
+		closeMenuBtn: document.querySelector('.btn__menu--close'),
 		menu: document.querySelector('.mob-menu'),
 		body: document.querySelector('body'),
 	};
